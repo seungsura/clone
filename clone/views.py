6b5481a92bc6f684b614c62347bd66d49a9dd8e0
seungsura <seungsura@gmail.com>
2023-03-07 00:10:29 +0000

    please...

--- clone/views.py
+++ clone/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-
+from content.models import Feed
 
 
 class Main(APIView):
     template_name = 'clone/main.html'
-    
+    feed_list = Feed.object.all()
+
     def get(self, request):
-        print("get으로 호출함")
-        return render(request, self.template_name)
+        print("get으로 호출")
+        return render(request, self.template_name, context=dict(feed_list=feed_list))
     
     
     def post(self, request):
-        print("post로 호출함")
+        print("post로 호출")
         return render(request, self.template_name)
